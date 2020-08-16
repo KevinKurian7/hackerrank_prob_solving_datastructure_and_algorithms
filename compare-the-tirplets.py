64de8dab61df7c1c10a95fd244a078849376b3c0
Kevin Kurian <kevinthathathil@gmail.com>
2020-08-16 12:05:30 +0530

    Add files via upload

---
+++ compare-the-tirplets.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/compare-the-triplets/problem?h_r=next-challenge&h_v=zen
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    j=list()
+    pa=0
+    pb=0
+    for i in range(3):
+        
+        if a[i]>b[i]:
+            pa=pa+1
+            pb=pb+0
+        elif a[i]==b[i]:
+            pa=pa+0
+            pb=pb+0
+        elif a[i]<b[i]:
+            pa=pa+0
+            pb=pb+1        
+    j=[pa,pb]
+    return j        
+        
+               
+   
+  
+
+                        
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
